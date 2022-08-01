90d2fd600301c39e4579ba272fd183fd8444bb7b
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-01 10:28:26 +0300

    header (no-adaptive)

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import './Header.css'
 
-import './Header.css';
+import { images } from '../../constants'
+import { SubHeading } from '../../components'
 
 const Header = () => (
-  <div>
-    Header
-  </div>
-);
+  <div className="header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="header-h1">The key to fine dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil fugit
+        aliquam amet alias! Rem, aliquid? Exercitationem architecto magni
+        accusantium error ullam quasi voluptatibus, harum ipsum. Aut
+        exercitationem odio illo sit?
+      </p>
+      <button type="button" className="custom__button">
+        Explore menu
+      </button>
+    </div>
 
-export default Header;
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="" />
+    </div>
+  </div>
+)
+
+export default Header
