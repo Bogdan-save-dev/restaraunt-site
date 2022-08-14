6c087a8fb827b60406eae4e775b866188a3bffa0
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-14 14:04:40 +0300

    video player

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../../constants'
+import './Intro.css'
 
-import './Intro.css';
+const Intro = () => {
+  const [play, setplay] = useState(false)
+  const vidRef = React.useRef()
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+  const handleVideo = () => {
+    setplay((prevPlay) => !prevPlay)
 
-export default Intro;
+    if (play) {
+      vidRef.current.pause()
+    } else {
+      vidRef.current.play()
+    }
+  }
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      ></video>
+      <div className="video__overlay flex__center">
+        <div
+          className="video__overlay-circle flex__center"
+          onClick={handleVideo}
+        >
+          {play ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
