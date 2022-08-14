18f3cc933d521f5baf96de335f503593e471d4d8
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-14 13:36:20 +0300

    chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from 'react'
 
-import './Chef.css';
+import './Chef.css'
+
+import { images } from '../../constants'
+import { SubHeading } from '../../components'
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
 
-export default Chef;
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="chef__content">
+        <div className="chef__content-qoute">
+          <img src={images.quote} alt="qoute" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quisquam
+            odit est itaque provident distinctio fuga, inventore ea autem, natus
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus,
+          amet commodi. Eligendi tempora dicta laboriosam quae, consectetur
+        </p>
+      </div>
+
+      <div className="chef__sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Fouder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+)
+
+export default Chef
