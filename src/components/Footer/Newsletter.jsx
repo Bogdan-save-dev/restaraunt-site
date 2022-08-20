26509926f499d056d3b0fb22bec89dd13cff566d
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-20 14:04:35 +0300

    newsletter component is completed

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
+import SubHeading from '../SubHeading/SubHeading'
 
-import './Newsletter.css';
+import './Newsletter.css'
 
 const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+  <div className="newsletter">
+    <div className="newsletter__heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest updates!</p>
+    </div>
 
-export default Newsletter;
+    <div className="newsletter__input flex__center">
+      <input type="email" placeholder="Enter your email" />
+      <button type="button" className="custom__button">
+        Subscribe
+      </button>
+    </div>
+  </div>
+)
+
+export default Newsletter
